05a0734fb5b3294b5f4a1b27eccd9ef6b0dcca3d
unknown <hamyhamza65@gmail.com>
2020-10-13 00:01:21 +0500

    init

---
+++ src/component/counter.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    //react update by calling the set state
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getbadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getbadgeClasses() {
+    let classes = "badge badge-";
+    classes += this.state.count === 0 ? "warning m-2" : "primary m-2";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+export default counter;
